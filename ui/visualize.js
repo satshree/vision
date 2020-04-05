14fefe18e3ff2c6fce40c2bf12ecb22a9d99801a
satshree <satshree.shrestha@gmail.com>
2020-04-05 17:32:17 +0545

    cleaned visualize

--- ui/visualize.js
+++ ui/visualize.js
@@ -42,30 +42,12 @@ function visualize(results) {
         ctx = document.getElementById("graphical").getContext("2d");
     
         let data = {
-            // labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
             labels: window.labels,
             responsive:true,
             datasets: [{
                 label: 'Total Devices',
-                // data: [12, 19, 3, 5, 2, 3],
                 data: window.data,
-                // backgroundColor: [
-                //     'rgba(255, 99, 132, 0.2)',
-                //     'rgba(54, 162, 235, 0.2)',
-                //     'rgba(255, 206, 86, 0.2)',
-                //     'rgba(75, 192, 192, 0.2)',
-                //     'rgba(153, 102, 255, 0.2)',
-                //     'rgba(255, 159, 64, 0.2)'
-                // ],
                 backgroundColor: window.backgroundColor,
-                // borderColor: [
-                //     'rgba(255, 99, 132, 1)',
-                //     'rgba(54, 162, 235, 1)',
-                //     'rgba(255, 206, 86, 1)',
-                //     'rgba(75, 192, 192, 1)',
-                //     'rgba(153, 102, 255, 1)',
-                //     'rgba(255, 159, 64, 1)'
-                // ],
                 borderColor: window.borderColor,
                 borderWidth: 1
             }]
