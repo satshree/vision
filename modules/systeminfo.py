4412c7e009ea5608f91a41b9555415517db0a483
satshree <satshree.shrestha@gmail.com>
2020-04-05 17:26:26 +0545

    removed old cli

--- modules/systeminfo.py
+++ modules/systeminfo.py
@@ -8,46 +8,62 @@ __author__ = "Satshree Shrestha"
 import socket
 import subprocess
 import sys
+from netifaces import gateways, AF_INET
 
 ################################################################################
 def get_ip_address():
     """ This will retrieve the IP address of the system. """
 
-    if sys.platform == 'win32':
-        # FOR WINDOWS
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-        # Runs 'ipconfig' on command prompt, decode output and split every word into list.
-        command = subprocess.check_output(["ipconfig"]).decode('utf-8').split()
-
-        # Run for loop from behind to get index position of WAN interface "Subnet" Mask.
-        for i in range((len(command)-1), 0, -1):
-            if command[i] == "Subnet":
-                index=i
-                break
-
-        # WAN interface IP address is placed right before Subnet Mask.
-        ip_address = command[index-1]
-    elif sys.platform == 'darwin':
-        # FOR macOS
-
+        sock.connect(('10.255.255.255', 1))
+        ip_address = sock.getsockname()[0]
+    except:
         try:
-            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            # Get hostname of the system
+            system_hostname = socket.gethostname()
 
-            sock.connect(('10.255.255.255', 1))
-            ip_address = sock.getsockname()[0]
+            # Get IP Address of the system
+            ip_address = socket.gethostbyname(system_hostname)
         except:
-            try:
-                # Get hostname of the system
-                system_hostname = socket.gethostname()
+            ip_address = '...... seems like Vision is unable to retrieve your IP Address.'
 
-                # Get IP Address of the system
-                ip_address = socket.gethostbyname(system_hostname)
-            except:
-                ip_address = '...... seems like Vision is unable to retrieve your IP Address.'
+    # if sys.platform == 'win32':
+    #     # FOR WINDOWS
 
-    else:
-        print('\nCannot identify your system. Vision works on Windows and macOS.\n')
-        return OSError
+    #     # Runs 'ipconfig' on command prompt, decode output and split every word into list.
+    #     command = subprocess.check_output(["ipconfig"]).decode('utf-8').split()
+
+    #     # Run for loop from behind to get index position of WAN interface "Subnet" Mask.
+    #     for i in range((len(command)-1), 0, -1):
+    #         if command[i] == "Subnet":
+    #             index=i
+    #             break
+
+    #     # WAN interface IP address is placed right before Subnet Mask.
+    #     ip_address = command[index-1]
+    # elif sys.platform == 'darwin':
+    #     # FOR macOS
+
+    #     try:
+    #         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    #         sock.connect(('10.255.255.255', 1))
+    #         ip_address = sock.getsockname()[0]
+    #     except:
+    #         try:
+    #             # Get hostname of the system
+    #             system_hostname = socket.gethostname()
+
+    #             # Get IP Address of the system
+    #             ip_address = socket.gethostbyname(system_hostname)
+    #         except:
+    #             ip_address = '...... seems like Vision is unable to retrieve your IP Address.'
+
+    # else:
+    #     print('\nCannot identify your system. Vision works on Windows and macOS.\n')
+    #     return OSError
 
     return ip_address
 
@@ -55,27 +71,30 @@ def get_ip_address():
 def get_default_gateway():
     """ This will retrieve the default gateway of the system. """
 
-    if sys.platform == 'win32':
-        # FOR WINDOWS
+    # if sys.platform == 'win32':
+    #     # FOR WINDOWS
 
-        # Run COMMAND PROMPT command, decode it and split it into list
-        cmd = subprocess.check_output(['ipconfig']).decode('utf-8').split()
+    #     # Run COMMAND PROMPT command, decode it and split it into list
+    #     cmd = subprocess.check_output(['ipconfig']).decode('utf-8').split()
 
-        # The last value will be gateway
-        gateway = cmd.pop()
-    elif sys.platform == 'darwin':
-        # FOR macOS
+    #     # The last value will be gateway
+    #     gateway = cmd.pop()
+    # elif sys.platform == 'darwin':
+    #     # FOR macOS
 
-        # Run terminal command to get default gateway
-        terminal_command = subprocess.check_output(['route', '-n', 'get', 'default']).decode('utf-8').split()
+    #     # Run terminal command to get default gateway
+    #     terminal_command = subprocess.check_output(['route', '-n', 'get', 'default']).decode('utf-8').split()
 
-        # Get index number for gateway
-        index = terminal_command.index('gateway:')
+    #     # Get index number for gateway
+    #     index = terminal_command.index('gateway:')
 
-        # Default gateway
-        gateway = terminal_command[index + 1]
-    else:
-        print('\nCannot identify your system. Vision works on Windows and macOS.\n')
-        return OSError
+    #     # Default gateway
+    #     gateway = terminal_command[index + 1]
+    # else:
+    #     print('\nCannot identify your system. Vision works on Windows and macOS.\n')
+    #     return OSError
 
-    return gateway
+    return gateways()['default'][AF_INET][0]
+
+def get_wlan_iface():
+    return gateways()['default'][AF_INET][-1]
